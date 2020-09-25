31d29abc7515c57bf2f4f7a002b2141724f04d35
lltim <48389438+lltim@users.noreply.github.com>
2020-09-24 22:20:21 -0400

    Updates

---
+++ precache-manifest.93f96d9342a6016ca3be0b1e1d991caf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "530a68b72ea015dd71f1e1817dd89aa5",
+    "url": "/github-jobs-app/index.html"
+  },
+  {
+    "revision": "3417db51acc9349676bd",
+    "url": "/github-jobs-app/static/js/2.5c8be4ca.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/github-jobs-app/static/js/2.5c8be4ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7297d7ea59673e01952d",
+    "url": "/github-jobs-app/static/js/main.bdb0ee54.chunk.js"
+  },
+  {
+    "revision": "8b73d2fe66d00593cdb4",
+    "url": "/github-jobs-app/static/js/runtime-main.1656721a.js"
+  }
+]);
